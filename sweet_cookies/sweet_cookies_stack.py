a240a5add1b341421475a6087f967dbe9db29896
John Long <johnyscrazy@gmail.com>
2020-05-13 23:35:33 +0200

    Add a Cookie Jar!

--- sweet_cookies/sweet_cookies_stack.py
+++ sweet_cookies/sweet_cookies_stack.py
@@ -3,17 +3,14 @@ from aws_cdk import aws_s3 as s3, core
 
 class SweetCookiesStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
-        super().__init__(
-            scope,
-            id
-            # , stack_name=kwargs["name"]
-        )
-        s3_host = s3.Bucket(
+        super().__init__(scope, id, stack_name=kwargs["name"])
+        s3.Bucket(
             self,
             id,
+            bucket_name=kwargs["name"],
             website_index_document="index.html",
             website_error_document="404.html",
-            public_read_access=True,
+            public_read_access=False,
         )
 
-        s3.CfnBucket(self, "CookieJar", bucket_name=s3_host.bucket_name)
+        # s3.CfnBucket(self, "CookieJar")
